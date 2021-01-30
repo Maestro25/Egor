d655c1b44595000e34fb2840aafc4e854c412c5a
Maestro25 <73071109+Maestro25@users.noreply.github.com>
2021-01-30 11:25:42 +0300

    Add files via upload

---
+++ message.php
@@ -0,0 +1,17 @@
+<?php 
+	if (isset($_POST['submit'])) {
+		$userName = $_POST['name'];
+		$userEmail = $_POST['email'];
+		$userPhone = $_POST['tel'];
+		$message = $_POST['text-field'];
+
+		$to = "ya.maestro25@yandex.ru";
+		$subject = "Письмо с сайта";
+		$headers = "От: ".$userEmail; 
+
+
+		mail($to, $subject, $headers, $message);
+		header("Location: index.html?mailsend");
+
+	}
+ ?>
