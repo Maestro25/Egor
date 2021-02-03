80666f6a034d6263b885e1a75c959254dbc5ff0b
Maestro25 <73071109+Maestro25@users.noreply.github.com>
2021-02-03 21:25:11 +0300

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -1,1 +1,20 @@
-const = document.querySelector('.')
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-list');
+	const navLinks = document.querySelectorAll('.nav-link');
+
+	burger.addEventListener('click', () => {
+		nav.classList.toggle('nav-active');
+
+		/* navLinks.forEach((link, index) => {
+		 	link.style.animation = `navLinkFade 0s ease forwards ${index/7}s`;
+		 }); */
+
+		burger.classList.toggle('open');
+	});
+
+
+}
+
+
+navSlide();
